78dafb540d5df5347f801d7624aa2a00f9b15718
Phan Minh Thi <thi@corsivalab.space>
2026-03-24 21:36:06 +0700

    update permission

--- src/main/java/org/example/jdbcusermanagement/controller/UserServlet.java
+++ src/main/java/org/example/jdbcusermanagement/controller/UserServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "userServlet", value = "/users")
@@ -128,17 +129,36 @@ public class UserServlet extends HttpServlet {
     }
 
 
-    private void insertUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+    private void insertUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String country = request.getParameter("country");
-        User user = new User(name, email, country);
-//        userDAO.insertUser(user);
-        userDAO.insertUserStore(user);
+
+        String add = request.getParameter("add");
+        String edit = request.getParameter("edit");
+        String delete = request.getParameter("delete");
+        String view = request.getParameter("view");
+        List<Integer> permissions = new ArrayList<>();
+        if (add != null){
+            permissions.add(1);
+        }
+        if (edit != null){
+            permissions.add(2);
+        }
+        if (delete != null){
+            permissions.add(3);
+        }
+        if (view != null){
+            permissions.add(4);
+        }
+
+        User newUser = new User(name, email, country);
+        //userDAO.insertUser(newUser);
+        userDAO.addUserTransaction(newUser, permissions);
         RequestDispatcher dispatcher = request.getRequestDispatcher("user/create.jsp");
         dispatcher.forward(request, response);
     }
-
     public void deleteUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
         int id = Integer.parseInt(request.getParameter("id"));
         userDAO.deleteUser(id);
